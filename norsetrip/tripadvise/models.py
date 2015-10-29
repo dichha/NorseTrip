26b538ecaa307012e32d1c099b5320cbf7c7c7eb
Dichha <raidi01@luther.edu>
2015-10-29 17:42:29 -0500

    models for the database table

--- norsetrip/tripadvise/models.py
+++ norsetrip/tripadvise/models.py
@@ -1,19 +1,117 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
-class Lodging(models.Model):
-	def __str__(self):
-		return self.lodge_name
+class Lodge(models.Model):
+	def __int__(self):
+		return self.lodgeId
 
-	#def __str__(self):
-	#	return self.lodge_address
-
-	# def __str__(self):
-	# 	return city
-
-	lodge_name = models.CharField(max_length = 200,db_column = "Name")
- 	#course_Id = models.ForeignKey(Course,verbose_name = "course_id")
+	# lodgeId = models.IntegerField(primary_key = True, db_column = "LodgeId")
+	lodgeId = models.AutoField(primary_key = True,db_column = "LodgeId")
+	#Autoincrement pk
+	lodge_name = models.CharField(max_length = 200,db_column = "Name")	
  	lodge_address = models.CharField(max_length = 200, db_column = "Address")
  	city = models.CharField(max_length = 100, db_column = "City")
  	country = models.CharField(max_length = 100, db_column = "Country")
- 	pub_date = models.DateTimeField(db_column = "Date");
+ 	#pub_date = models.DateTimeField(db_column = "Date")
+ 	lodge_url = models.URLField(db_column = "URL")
+ 	lodge_descrip = models.TextField(db_column = "Description")
+ 	average_rating = models.IntegerField(db_column = "Average Rating", default = 100)
+
+class Course(models.Model):
+
+	def __int__(self):
+		return self.courseId
+
+
+	TERM = (
+	('JTERM','JTERM'),
+	('SUMMER','SUMMER'),
+	('YEAR','YEAR'),
+	('SEMESTER',"SEMESTER"),
+	)
+
+
+	# courseId = models.IntegerField(primary_key = True, db_column = "CourseId")
+	courseId = models.IntegerField(primary_key = True, db_column = "CourseId")
+	name = models.CharField(max_length = 200, db_column = "Name")
+	dept = models.CharField(max_length = 200, db_column = "Department")
+	prof = models.CharField(max_length = 200, db_column = "Professor")
+	year_offered = models.IntegerField(db_column = "Year Offered")
+	term = models.CharField(max_length = 8, choices = TERM, default = 'JTERM')
+	
+
+class Course_Lodge_Assignment(models.Model):
+	def __int__(self):
+		return self.lodge_Id
+
+	lodgeAssignId = models.AutoField(primary_key = True, db_column = "LodgeAssignId")
+	course_Id = models.ForeignKey(Course, verbose_name = "CourseId FK")
+	lodge_Id = models.ForeignKey(Lodge, verbose_name = "LodgeId FK")
+
+class User(models.Model):
+	def __str__(self):
+		return self.userId
+	userId = models.EmailField(db_column = "UserId", primary_key = True)
+	ROLE_CHOICES = (('PROFESSOR', 'PROFESSOR'),
+					('STUDENT','STUDENT'),
+					('ALUMNI', 'ALUMNI'),
+					('FACULTY', 'FACULTY'),
+		)
+	role = models.CharField(max_length = 9, choices = ROLE_CHOICES, db_column = "ROLE")
+
+class Review(models.Model):
+	def __int__(self):
+		return self.reviewId
+	reviewId = models.AutoField(primary_key = True, db_column = "ReviewId")
+	lodge_Id = models.ForeignKey(Lodge, db_column = "LodgeID FK")
+	user_Id = models.ForeignKey(User, db_column = "UserId FK")
+	ONE = '1'
+	TWO = '2'
+	THREE = '3'
+	FOUR = '4'
+	FIVE = '5'
+	
+	RATING_CHOICES = ((ONE,"1"),
+					  (TWO,"2"),
+					  (THREE,"3"),
+					  (FOUR,"4"),
+					  (FIVE,"5"),
+		)
+	rating = models.CharField(choices = RATING_CHOICES, db_column = "Rating", max_length = 1)
+	VC = '1'
+	PC = '2'
+	AVG = '3'
+	PE = '4'
+	VE = '5'
+	COST_CHOICES = ((VC, "Very Cheap"),
+					(PC, "Pretty Cheap"),
+					(AVG, "Average"),
+					(PE, "Pretty Expensive"),
+					(VE, "Very Expensive"),
+		)
+	cost = models.CharField(choices = COST_CHOICES, db_column = "Cost", max_length = 16)
+	comment = models.TextField(db_column = "Comment")
+	#pub_date = models.DateTimeField(db_column = "Date")
+
+class Course_Assignment(models.Model):
+	def __int__(self):
+		return self.courseAssignId
+
+	courseAssignId = models.AutoField(primary_key = True, db_column = "Course_AssignmentId")
+	course_Id = models.ForeignKey(Course, db_column = "CourseId FK")
+	user_Id = models.ForeignKey(User, db_column = "UserId FK")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
